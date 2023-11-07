0bec53d5ea2d31898fef5c9c9777e43742261edb
Taras Pelyno <tarasfall@gmail.com>
2023-11-07 22:32:30 +0200

    Update

--- src/app/components/all.module.ts
+++ src/app/components/all.module.ts
@@ -1,30 +1,36 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { ImageViewerComponent } from "./image-viewer/image-viewer.component";
-import {MatButtonModule} from '@angular/material/button';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
-import { FormsModule } from "@angular/forms";
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatDialogModule} from '@angular/material/dialog';
-import { ChooseFromCloudModalComponent } from "./choose-from-cloud-modal/choose-from-cloud-modal.component";
-import { ResultsInCloudModalComponent } from "./results-in-cloud-modal/results-in-cloud-modal.component";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ImageViewerComponent } from './image-viewer/image-viewer.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ChooseFromCloudModalComponent } from './choose-from-cloud-modal/choose-from-cloud-modal.component';
+import { ResultsInCloudModalComponent } from './results-in-cloud-modal/results-in-cloud-modal.component';
 
 @NgModule({
-    exports:[ImageViewerComponent, ChooseFromCloudModalComponent, ResultsInCloudModalComponent],
-    declarations: [
-        ImageViewerComponent, ChooseFromCloudModalComponent, ResultsInCloudModalComponent
-    ],
-    imports: [
-      BrowserModule,
-      MatButtonModule,
-      MatDividerModule,
-      MatInputModule,
-      MatIconModule,
-      FormsModule,
-      MatSlideToggleModule,
-      MatDialogModule
-    ],
-  })
-  export class AllModule { }
+  exports: [
+    ImageViewerComponent,
+    ChooseFromCloudModalComponent,
+    ResultsInCloudModalComponent,
+  ],
+  declarations: [
+    ImageViewerComponent,
+    ChooseFromCloudModalComponent,
+    ResultsInCloudModalComponent,
+  ],
+  imports: [
+    BrowserModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    MatSlideToggleModule,
+    MatDialogModule,
+  ],
+})
+export class AllModule {}
--- src/app/components/image-viewer/image-viewer.component.ts
+++ src/app/components/image-viewer/image-viewer.component.ts
@@ -2,20 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environment';
 import { createApi } from 'unsplash-js';
 import { Random } from 'unsplash-js/dist/methods/photos/types';
-import { BlobServiceClient } from "@azure/storage-blob";
+import { BlobServiceClient } from '@azure/storage-blob';
 import { MatDialog } from '@angular/material/dialog';
 import { ChooseFromCloudModalComponent } from '../choose-from-cloud-modal/choose-from-cloud-modal.component';
+import { kmeans } from 'ml-kmeans';
 import { ResultsInCloudModalComponent } from '../results-in-cloud-modal/results-in-cloud-modal.component';
 
 @Component({
   selector: 'image-viewer',
   templateUrl: './image-viewer.component.html',
-  styleUrls: ['./image-viewer.component.css']
+  styleUrls: ['./image-viewer.component.css'],
 })
 export class ImageViewerComponent implements OnInit {
-  searchText: string = "";
-  admission: number = 50;
-  photos: Random[] = [{urls:{small:""}} as Random, {urls:{small:""}} as Random];
+  searchText: string = '';
+  admission: number = 5;
+  clusters: number = 5;
+  iterations: number = 100;
+  photos: Random[] = [
+    { urls: { small: '' } } as Random,
+    { urls: { small: '' } } as Random,
+  ];
   result: string = '';
   unsplash = createApi({
     accessKey: environment.unsplashAccessKey,
@@ -26,17 +32,17 @@ export class ImageViewerComponent implements OnInit {
 
   constructor(public dialog: MatDialog) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getImages() {
+    this.unsplash.photos
+      .getRandom({ query: this.searchText, count: 2, orientation: 'squarish' })
+      .then((response) => {
+        this.photos = response.response as Random[];
+      });
   }
 
-  getImages(){
-    this.unsplash.photos.getRandom({query: this.searchText, count: 2}).then(response => {
-      this.photos = response.response as Random[];
-
-    });
-  }
-
-  swap(){
+  swap() {
     console.log(this.isGrayed);
     this.photos.reverse();
   }
@@ -47,24 +53,28 @@ export class ImageViewerComponent implements OnInit {
     const ctx = canvas.getContext('2d');
     const img = new Image();
     img.src = this.photos[0].urls.small;
-    img.crossOrigin = "Anonymous";
+    img.crossOrigin = 'Anonymous';
     img.onload = () => {
       canvas.width = img.width;
       canvas.height = img.height;
       ctx!.drawImage(img, 0, 0);
       const imageData = ctx!.getImageData(0, 0, canvas.width, canvas.height);
-      
+
       const canvas2 = document.createElement('canvas');
       const ctx2 = canvas2.getContext('2d');
       const img2 = new Image();
       img2.src = this.photos[1].urls.small;
-      img2.crossOrigin = "Anonymous";
+      img2.crossOrigin = 'Anonymous';
       img2.onload = () => {
-        
         canvas2.width = img2.width;
         canvas2.height = img2.height;
         ctx2!.drawImage(img2, 0, 0);
-        const imageData2 = ctx2!.getImageData(0, 0, canvas2.width, canvas2.height);
+        const imageData2 = ctx2!.getImageData(
+          0,
+          0,
+          canvas2.width,
+          canvas2.height
+        );
 
         let result = this.compareImages(imageData, imageData2);
 
@@ -77,115 +87,176 @@ export class ImageViewerComponent implements OnInit {
     };
   }
 
+  compareKmeans() {
+    const canvas = document.createElement('canvas');
+    canvas.id = 'result-image';
+    const ctx = canvas.getContext('2d');
+    const img = new Image();
+    img.src = this.photos[0].urls.small;
+    img.crossOrigin = 'Anonymous';
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx!.drawImage(img, 0, 0);
+      const imageData = ctx!.getImageData(0, 0, canvas.width, canvas.height);
+
+      const canvas2 = document.createElement('canvas');
+      const ctx2 = canvas2.getContext('2d');
+      const img2 = new Image();
+      img2.src = this.photos[1].urls.small;
+      img2.crossOrigin = 'Anonymous';
+      img2.onload = () => {
+        canvas2.width = img2.width;
+        canvas2.height = img2.height;
+        ctx2!.drawImage(img2, 0, 0);
+        const imageData2 = ctx2!.getImageData(
+          0,
+          0,
+          canvas2.width,
+          canvas2.height
+        );
+
+        let result = this.calcKMeans(imageData, imageData2);
+
+        ctx!.putImageData(result, 0, 0);
+        let resultElement = document.querySelector('#result');
+        resultElement!.innerHTML = '';
+        resultElement!.appendChild(canvas);
+        this.isCompared = true;
+      };
+    };
+  }
+
   uploadImage(number: number) {
-    (document.querySelector(`#uploadImage${number}`) as HTMLInputElement)!.click();
+    (document.querySelector(
+      `#uploadImage${number}`
+    ) as HTMLInputElement)!.click();
   }
 
   onUploadImageChange($event: any, number: number) {
     const file = $event.target!.files![0];
-    
+
     const reader = new FileReader();
     reader.onload = async () => {
-      this.photos[number-1]!.urls.small = reader.result as string;
-      
-      const containerClient = this.blobServiceClient.getContainerClient('imager');
+      this.photos[number - 1]!.urls.small = reader.result as string;
+
+      const containerClient =
+        this.blobServiceClient.getContainerClient('imager');
       const content = reader.result as string;
-      const blobName = "newblob" + new Date().getTime();
+      const blobName = 'newblob' + new Date().getTime();
       const blockBlobClient = containerClient.getBlockBlobClient(blobName);
       await blockBlobClient.upload(content, content.length);
-    }
-    
+    };
+
     if (file) {
       reader.readAsDataURL(file);
     }
   }
 
-  async save(){
+  async save() {
     var link = document.createElement('a');
     link.download = 'Imager_ComparisonResult.png';
-    link.href = (document.querySelector(`#result-image`) as HTMLCanvasElement).toDataURL();
+    link.href = (
+      document.querySelector(`#result-image`) as HTMLCanvasElement
+    ).toDataURL();
     link.click();
 
-    const containerClient = this.blobServiceClient.getContainerClient('results');
+    const containerClient =
+      this.blobServiceClient.getContainerClient('results');
     const content = link.href;
-    const blobName = "newblob" + new Date().getTime();
+    const blobName = 'newblob' + new Date().getTime();
     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
     await blockBlobClient.upload(content, content.length);
   }
 
-  chooseFromCloudModal(enterAnimationDuration: string, exitAnimationDuration: string){
+  chooseFromCloudModal(
+    enterAnimationDuration: string,
+    exitAnimationDuration: string
+  ) {
     const modal = this.dialog.open(ChooseFromCloudModalComponent, {
       width: '75vw',
       enterAnimationDuration,
       exitAnimationDuration,
     });
 
-    modal.afterClosed().subscribe(result => {
-      if (result){
-        if (result[0] && result[0] !== ''){
+    modal.afterClosed().subscribe((result) => {
+      if (result) {
+        if (result[0] && result[0] !== '') {
           this.photos[0]!.urls.small = result[0];
         }
-        
-        if (result[1] && result[1] !== ''){
+
+        if (result[1] && result[1] !== '') {
           this.photos[1]!.urls.small = result[1];
         }
       }
     });
   }
 
-  results(enterAnimationDuration: string, exitAnimationDuration: string){
+  results(enterAnimationDuration: string, exitAnimationDuration: string) {
     this.dialog.open(ResultsInCloudModalComponent, {
       width: '75vw',
       enterAnimationDuration,
-      exitAnimationDuration
+      exitAnimationDuration,
     });
   }
 
   private grayScale(imageData: ImageData) {
     const pixels = imageData.data;
     for (let i = 0; i < pixels.length; i += 4) {
-      let avg = (pixels[i] + pixels[i+1] + pixels[i+2]) / 3;
+      let avg = (pixels[i] + pixels[i + 1] + pixels[i + 2]) / 3;
       pixels[i] = avg;
-      pixels[i+1] = avg;
-      pixels[i+2] = avg;
+      pixels[i + 1] = avg;
+      pixels[i + 2] = avg;
     }
   }
 
-  private compareImages(image: ImageData, image2: ImageData): ImageData{
-    if(this.isGrayed){
+  private compareImages(image: ImageData, image2: ImageData): ImageData {
+    if (this.isGrayed) {
       this.grayScale(image);
       this.grayScale(image2);
     }
 
     const pixels = image.data;
-    for(let i = 0; i < pixels.length; i+=4){
+    for (let i = 0; i < pixels.length; i += 4) {
       let y = this.calculateRelativeLuminance(pixels, i) * 100;
       let y2 = this.calculateRelativeLuminance(image2.data, i) * 100;
 
-      if(Math.abs(y - y2) > this.admission) {
-        if (y - y2 >= 0){
+      if (Math.abs(y - y2) > this.admission) {
+        if (y - y2 >= 0) {
           pixels[i] = 255;
-          pixels[i+2] = 0;
-        }
-        else{
+          pixels[i + 2] = 0;
+        } else {
           pixels[i] = 0;
-          pixels[i+2] = 255;
+          pixels[i + 2] = 255;
         }
-        
-        pixels[i+1] = 0;
-        pixels[i+3] = 255;
+
+        pixels[i + 1] = 0;
+        pixels[i + 3] = 255;
       }
     }
-    
+
     return image;
   }
-  
+
   // https://en.wikipedia.org/wiki/Relative_luminance
-  private calculateRelativeLuminance(pixels: Uint8ClampedArray, i: number): number {
-    let y = 0.2126*this.sRGBtoLin(pixels[i]/255) +
-      0.7152*this.sRGBtoLin(pixels[i+1]/255) + 
-      0.0722*this.sRGBtoLin(pixels[i+2]/255); 
-      
+  private calculateRelativeLuminance(
+    pixels: Uint8ClampedArray,
+    i: number
+  ): number {
+    let y =
+      0.2126 * this.sRGBtoLin(pixels[i] / 255) +
+      0.7152 * this.sRGBtoLin(pixels[i + 1] / 255) +
+      0.0722 * this.sRGBtoLin(pixels[i + 2] / 255);
+
+    return y;
+  }
+
+  private calculateRelativeLuminance2(pixels: number[]): number {
+    let y =
+      0.2126 * this.sRGBtoLin(pixels[0] / 255) +
+      0.7152 * this.sRGBtoLin(pixels[1] / 255) +
+      0.0722 * this.sRGBtoLin(pixels[2] / 255);
+
     return y;
   }
 
@@ -194,10 +265,86 @@ export class ImageViewerComponent implements OnInit {
     // Send this function a decimal sRGB gamma encoded color value
     // between 0.0 and 1.0, and it returns a linearized value.
 
-    if ( colorChannel <= 0.04045 ) {
-        return colorChannel / 12.92;
+    if (colorChannel <= 0.04045) {
+      return colorChannel / 12.92;
     } else {
-        return Math.pow(((colorChannel + 0.055) / 1.055), 2.4);
+      return Math.pow((colorChannel + 0.055) / 1.055, 2.4);
     }
-}
+  }
+
+  private calcKMeans(image: ImageData, image2: ImageData): ImageData {
+    if (this.isGrayed) {
+      this.grayScale(image);
+      this.grayScale(image2);
+    }
+
+    let imageData1 = image.data;
+    let imageData2 = image2.data;
+
+    if (imageData1.length !== imageData2.length) {
+      alert('Image data sizes do not match.');
+      throw new Error('Image data sizes do not match.');
+    }
+
+    const pixelCount = imageData1.length / 4;
+
+    const pixels1: number[][] = [];
+    const pixels2: number[][] = [];
+
+    for (let i = 0; i < pixelCount; i++) {
+      const index = i * 4;
+      pixels1.push([
+        imageData1[index],
+        imageData1[index + 1],
+        imageData1[index + 2],
+        imageData1[index + 3],
+      ]);
+      pixels2.push([
+        imageData2[index],
+        imageData2[index + 1],
+        imageData2[index + 2],
+        imageData2[index + 3],
+      ]);
+    }
+
+    const clusteringResult = kmeans(pixels1, this.clusters, {
+      maxIterations: this.iterations,
+      seed: 13,
+    });
+
+    const clusteringResult2 = kmeans(pixels2, this.clusters, {
+      maxIterations: this.iterations,
+      seed: 13,
+    });
+
+    const clusters1 = clusteringResult.clusters;
+    const clusters2 = clusteringResult2.clusters;
+
+    for (let i = 0; i < pixelCount; i++) {
+      const index = i * 4;
+
+      let y =
+        this.calculateRelativeLuminance2([
+          clusteringResult.centroids[clusters1[i]][0],
+          clusteringResult.centroids[clusters1[i]][1],
+          clusteringResult.centroids[clusters1[i]][2],
+        ]) * 100;
+
+      let y2 =
+        this.calculateRelativeLuminance2([
+          clusteringResult2.centroids[clusters2[i]][0],
+          clusteringResult2.centroids[clusters2[i]][1],
+          clusteringResult2.centroids[clusters2[i]][2],
+        ]) * 100;
+
+      if (Math.abs(y - y2) > this.admission) {
+        imageData1[index] = 255;
+        imageData1[index + 1] = 0;
+        imageData1[index + 2] = 0;
+        imageData1[index + 3] = 255;
+      }
+    }
+
+    return image;
+  }
 }
